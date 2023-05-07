27639ffe75cf3c1e2cee24c96a4d292b9f1217fa
Марис <122129172+Maris-ITMO@users.noreply.github.com>
2023-05-07 22:16:41 +0300

    Add project files.

---
+++ TEMA_3_DZ_5_1_Library/SelectionUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace TEMA_3_DZ_5_1_Library
+{
+    public class SelectionUtils
+    {
+        public static List<Pipe> PipeCount(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Pipe> fInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PipeCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Pipe>()
+                .ToList();
+
+            return fInstances;
+
+        }
+
+        public static List<Wall> WallVolume(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Wall> fInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType()
+                .Cast<Wall>()
+                .ToList();
+
+            return fInstances;
+
+        }
+
+        public static List<FamilyInstance> DoorCount(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> doors = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            return doors;
+        }
+    }
+}
